693a56ae77056989ef340d0e02008ac920a7e9bd
tikastikas <up202306445@up.pt>
2024-11-26 11:20:25 +0000

    test commit

---
+++ 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    for (int i = 0; i < 4; i++)
+        fork();
+    printf("PID: %d \n", getpid());    
+    exit(EXIT_SUCCESS);
+}
+
+// 16 processes in total
+// why?
